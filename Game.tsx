98c14e91e0db861069efac320adbc124be3ddf05
steven payne <djnosey@hotmail.co.uk>
2021-02-02 10:15:20 +0100

    change to typescript

--- Game.tsx
+++ Game.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Game() {
-  let target = 10 + Math.floor(40 * Math.random());
+  let target : number = 10 + Math.floor(40 * Math.random());
 
   return (
     <View style={styles.container}>
